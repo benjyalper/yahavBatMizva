a6f405be944f442f22b8860ba7ec1dd39d268ff8
benjyalper <benjyalper@hotmail.com>
2023-07-14 15:27:46 +0300

    animate img

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
 $('.one').on("click", function () {
     $('.one').addClass('animate__flipInY');
+});
+
+$('.two').on("click", function () {
+    $('.two').addClass('animate__flipInY');
+    $('img').css('visibility', 'visible');
+    setTimeout(
+        function () {
+            $('.two').addClass('animate__zoomOut');
+        }, 3000);
+    setTimeout(
+        function () {
+            $('.two').removeClass('animate__zoomOut');
+            $('.two').addClass('animate__zoomIn');
+            $('img').css('visibility', 'hidden');
+        }, 4000);
+
+
+
 }); 
