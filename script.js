19d5d615727b57a13ba84b5db1b34af3bf9f83a7
benjyalper <benjyalper@hotmail.com>
2023-07-15 15:36:07 +0300

    animate img

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 $('.one').on("click", function () {
     $('.one').addClass('animate__flipInY');
+    $('.one>img').css('visibility', 'visible');
+    setTimeout(
+        function () {
+            $('.one').addClass('animate__zoomOut');
+        }, 2500);
+    setTimeout(
+        function () {
+            $('.one2').css('visibility', 'visible');
+            $('.one2').addClass('animate__zoomIn');
+        }, 3000);
 });
 
 
@@ -12,9 +22,8 @@ $('.two').on("click", function () {
         }, 2500);
     setTimeout(
         function () {
-            $('.two').removeClass('animate__zoomOut');
-            $('.two').addClass('animate__zoomIn');
-            $('img').css('visibility', 'hidden');
+            $('.two2').css('visibility', 'visible');
+            $('.two2').addClass('animate__zoomIn');
         }, 3000);
 });
 
@@ -34,6 +43,34 @@ $('.three').on('click', function () {
             // $('.three').after(`<div class="item three2 animate__animated animate__zoomIn" style="grid-area: 1 / 3 / 4 / 4; background-color: #FFDEB4;">Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga incidunt accusantium
             // magnam ut at architecto, nisi eligendi? Molestiae recusandae, sunt ducimus quo eaque cupiditate
             // voluptates nostrum? Vel aut praesentium tempore.</div>`);
+        }, 3000);
+});
+
+$('.four').on("click", function () {
+    $('.four').addClass('animate__flipInY');
+    $('.four>img').css('visibility', 'visible');
+    setTimeout(
+        function () {
+            $('.four').addClass('animate__zoomOut');
+        }, 2500);
+    setTimeout(
+        function () {
+            $('.four2').css('visibility', 'visible');
+            $('.four2').addClass('animate__zoomIn');
+        }, 3000);
+});
+
+$('.five').on("click", function () {
+    $('.five').addClass('animate__flipInY');
+    $('.five>img').css('visibility', 'visible');
+    setTimeout(
+        function () {
+            $('.five').addClass('animate__zoomOut');
+        }, 2500);
+    setTimeout(
+        function () {
+            $('.five2').css('visibility', 'visible');
+            $('.five2').addClass('animate__zoomIn');
         }, 3000);
 });
 
